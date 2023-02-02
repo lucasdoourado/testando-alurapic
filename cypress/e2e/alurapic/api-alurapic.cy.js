21399a5f7fb7a41216fa0e8774c98f8e7f83d1e2
lucasdoourado <lucas.dourado.aguiar@icloud.com>
2023-02-02 16:12:46 -0300

    requisicao login usuario tratando dados sensiveis

--- cypress/e2e/alurapic/api-alurapic.cy.js
+++ cypress/e2e/alurapic/api-alurapic.cy.js
@@ -12,4 +12,21 @@ describe("Buscar fotos e dados", () => {
       expect(resposta.body[0].description).to.be.equal("Farol iluminado");
     });
   });
+
+  it("fazer login do Flávio", () => {
+    cy.request({
+      method: "POST",
+      url: "https://apialurapic.herokuapp.com/user/login",
+      body: Cypress.env(),
+    }).then((resposta) => {
+      expect(resposta.status).to.be.equal(200);
+      expect(resposta.body).is.not.empty;
+      expect(resposta.body).to.have.property("name");
+      expect(resposta.body.name).to.be.equal("flavio");
+      expect(resposta.body).to.have.property("id");
+      expect(resposta.body.id).to.be.equal(1);
+      expect(resposta.body).to.have.property("email");
+      expect(resposta.body.email).to.be.equal("flavio@alurapic.com.br");
+    });
+  });
 });
