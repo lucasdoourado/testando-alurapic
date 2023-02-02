a01772dd8cf501df90c2865e223a99e08a61db7e
lucasdoourado <lucas.dourado.aguiar@icloud.com>
2023-02-02 09:23:53 -0300

    verifica mensagens de validacao no registro

---
+++ cypress/e2e/alurapic/alurapic.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe("Login e registro de usuarios alura pic", () => {
+  beforeEach(() => {
+    cy.visit("http://alura-fotos.herokuapp.com/");
+  });
+  it("verifica mensagens de validacao no registro", () => {
+    cy.contains("a", "Register now").click();
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Email is required!").should("be.visible");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Full name is required!").should("be.visible");
+    cy.contains("ap-vmessage", "User name is required!").should("be.visible");
+    cy.contains("ap-vmessage", "Password is required!").should("be.visible");
+  });
+});
